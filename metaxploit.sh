fe405ebf12aea189aaaa759a42b123473439df9b
Anonycodexia <69398110+Anonycodexia@users.noreply.github.com>
2023-11-10 23:51:54 +0530

    metaxploit.sh

--- metaxploit.sh
+++ metaxploit.sh
@@ -7,15 +7,12 @@ center() {
   printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
 }
 
-center " Loading..."
+center "Loading..."
 source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
 
 echo
 center "*** Dependencies installation..."
 
-rm $PREFIX/etc/apt/sources.list.d/*
-
-pkg upgrade -y -o Dpkg::Options::="--force-confnew"
 pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
 
 python3 -m pip install --upgrade pip
@@ -31,14 +28,16 @@ rm -rf $PREFIX/opt/metasploit-framework
 
 echo
 center "*** Downloading..."
+mkdir -p $PREFIX/opt
 cd $PREFIX/opt
 git clone https://github.com/rapid7/metasploit-framework.git --depth=1
 
 echo
 center "*** Installation..."
-cd $PREFIX/opt/metasploit-framework
+cd $PREFIX/opt/metasploit-framework || exit
 
-gem install bundler
+gem install bundler -v 2.4.22
+
 declare NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
 gem install nokogiri -v $NOKOGIRI_VERSION -- --use-system-libraries
 
@@ -50,17 +49,18 @@ bundle update --bundler
 bundle install -j$(nproc --all)
 
 if [ -e $PREFIX/bin/msfconsole ]; then
-  rm $PREFIX/bin/msfconsole
+    rm $PREFIX/bin/msfconsole
 fi
 if [ -e $PREFIX/bin/msfvenom ]; then
-  rm $PREFIX/bin/msfvenom
+    rm $PREFIX/bin/msfvenom
 fi
 if [ -e $PREFIX/bin/msfrpcd ]; then
-  rm $PREFIX/bin/msfrpcd
+    rm $PREFIX/bin/msfrpcd
 fi
-ln -s $PREFIX/opt/metasploit-framework/msfconsole $PREFIX/bin/
-ln -s $PREFIX/opt/metasploit-framework/msfvenom $PREFIX/bin/
-ln -s $PREFIX/opt/metasploit-framework/msfrpcd $PREFIX/bin/
+
+ln -sf $PREFIX/opt/metasploit-framework/msfconsole $PREFIX/bin/
+ln -sf $PREFIX/opt/metasploit-framework/msfvenom $PREFIX/bin/
+ln -sf $PREFIX/opt/metasploit-framework/msfrpcd $PREFIX/bin/
 
 termux-elf-cleaner $PREFIX/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
 
@@ -68,12 +68,16 @@ echo
 center "*"
 echo -e "\033[32m Suppressing Warnings\033[0m"
 
-sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
-sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.1/lib/logging/diagnostic_context.rb
+[ -e $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb ] && sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
+
+[ -e $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.1/lib/logging/diagnostic_context.rb ] && sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.1/lib/logging/diagnostic_context.rb
 
 echo
 center "*"
 echo -e "\033[32m Installation complete. \n Launch metasploit by executing: msfconsole\033[0m"
 center "*"
-echo "Please Subscribe to Youtube Channel Cybereveal"
-rm -rf $HOME/MetaXploit
+center "*"
+echo -e "\033[32m Please Subscribe to YouTube Channel Cybereveal\033[0m"
+center "*"
+rm -rf $HOME/MetaXploit $HOME/metaxploit.sh
+

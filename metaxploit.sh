e4381ae5192764874b8eb1de96fb0e4ec5959697
Anonycodexia <69398110+Anonycodexia@users.noreply.github.com>
2023-11-10 14:47:10 +0530

    metaxploit.sh

--- metaxploit.sh
+++ metaxploit.sh
@@ -76,6 +76,6 @@ center "*"
 echo -e "\033[32m Installation complete. \n Launch metasploit by executing: msfconsole\033[0m"
 center "*"
 
-rm -rf $HOME/metasploit.sh
+rm -rf $HOME/metaxploit.sh
 
 echo "Please Subscribe to Youtube Channel Cybereveal"
